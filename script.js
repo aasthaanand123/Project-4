980ed58ff0c0912a758e39613642898b05e8159a
Aastha <aasthaanand193@hotmail.com>
2021-06-14 21:26:08 +0530

    js functionality(toggle)

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 let questions = document.querySelectorAll(".faq__quest");
 questions.forEach((q) =>
   q.addEventListener("click", function () {
+    questions.forEach((ques) => {
+      let a = ques.nextElementSibling;
+      if (!a.classList.contains("hide") && ques !== q) {
+        a.classList.add("hide");
+        ques.classList.remove("focus");
+        ques.children[1].classList.remove("rotate");
+      }
+    });
     q.children[1].classList.toggle("rotate");
-    let answer = this.nextElementSibling;
     q.classList.toggle("focus");
+    let answer = this.nextElementSibling;
     answer.classList.toggle("hide");
   })
 );
